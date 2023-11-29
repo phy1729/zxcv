b918f78f7a435dff84538d9a29dd31d6264465aa
Matthew Martin <phy1729@gmail.com>
2023-11-28 19:12:19 -0600

    github: Add issues URL handling

--- src/github.rs
+++ src/github.rs
@@ -1,8 +1,15 @@
 use anyhow::bail;
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
 use url::Url;
 
 use crate::process_generic;
 use crate::Content;
+use crate::Post;
+use crate::PostThread;
+use crate::TextType;
+
+const API_BASE: &str = "https://api.github.com";
 
 pub(crate) fn process(url: &mut Url) -> anyhow::Result<Content> {
     let path_segments: Vec<_> = url
@@ -15,9 +22,54 @@ pub(crate) fn process(url: &mut Url) -> anyhow::Result<Content> {
             url.set_path(&(url.path().to_owned() + ".patch"));
         }
         process_generic(url)
+    } else if path_segments.len() == 4 && path_segments[2] == "issues" {
+        let issue: Issue = request(&format!("{API_BASE}/repos{}", url.path()))?;
+        let comments: Vec<Comment> = request(&issue.comments_url)?;
+
+        Ok(Content::Text(TextType::PostThread(PostThread {
+            before: vec![],
+            main: Post {
+                author: issue.user.login,
+                body: issue.body,
+            },
+            after: comments
+                .into_iter()
+                .map(|c| Post {
+                    author: c.user.login,
+                    body: c.body,
+                })
+                .collect(),
+        })))
     } else {
         bail!("Unknown GitHub URL");
     }
+}
+
+#[allow(clippy::result_large_err)]
+fn request<T: DeserializeOwned>(url: &str) -> Result<T, ureq::Error> {
+    Ok(ureq::get(url)
+        .set("Accept", "application/vnd.github+json")
+        .set("X-GitHub-Api-Version", "2022-11-28")
+        .call()?
+        .into_json()?)
+}
+
+#[derive(Debug, Deserialize)]
+struct Comment {
+    body: String,
+    user: User,
+}
+
+#[derive(Debug, Deserialize)]
+struct Issue {
+    body: String,
+    comments_url: String,
+    user: User,
+}
+
+#[derive(Debug, Deserialize)]
+struct User {
+    login: String,
 }
 
 pub(crate) mod gist {
