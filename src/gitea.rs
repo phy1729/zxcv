114bbb6701fde38869f8f281d5f1bb9423690648
Matthew Martin <phy1729@gmail.com>
2024-01-09 21:45:42 -0600

    gitea: Refactor path parsing

--- src/gitea.rs
+++ src/gitea.rs
@@ -1,4 +1,5 @@
 use anyhow::bail;
+use anyhow::Context;
 use base64::Engine;
 use scraper::Html;
 use serde::Deserialize;
@@ -8,6 +9,37 @@ use url::Url;
 use crate::select_single_element;
 use crate::Content;
 use crate::TextType;
+
+#[derive(Debug, PartialEq)]
+enum Path<'a> {
+    Src(&'a str, &'a str, &'a str, &'a str),
+}
+
+fn parse_path(url: &Url) -> Option<Path<'_>> {
+    let path_segments: Vec<_> = url
+        .path_segments()
+        .unwrap_or_else(|| "".split('/'))
+        .collect();
+
+    Some(if path_segments.len() >= 6 && path_segments[2] == "src" {
+        Path::Src(
+            path_segments[0],
+            path_segments[1],
+            url.path()
+                .split_at(
+                    url.path()
+                        .match_indices('/')
+                        .nth(5)
+                        .expect("path_segments len checked above")
+                        .0,
+                )
+                .1,
+            path_segments[4],
+        )
+    } else {
+        return None;
+    })
+}
 
 pub(crate) fn process(agent: &Agent, url: &Url, tree: &Html) -> Option<anyhow::Result<Content>> {
     if select_single_element(tree, "meta[name=\"keywords\"]")
@@ -19,34 +51,27 @@ pub(crate) fn process(agent: &Agent, url: &Url, tree: &Html) -> Option<anyhow::R
     }
 
     Some((|| {
-        let path_segments: Vec<_> = url
-            .path_segments()
-            .unwrap_or_else(|| "".split('/'))
-            .collect();
+        let path = parse_path(url).context("Unknown Gitea URL")?;
         let api_base = url.join("/api/v1/")?;
 
-        if path_segments.len() >= 6 && path_segments[2] == "src" {
-            let path = path_segments[5..].join("/");
-            let content: ContentsResponse = agent
-                .request_url(
-                    "GET",
-                    &api_base.join(&format!(
-                        "repos/{}/{}/contents/{path}",
-                        path_segments[0], path_segments[1]
-                    ))?,
-                )
-                .query("ref", path_segments[4])
-                .call()?
-                .into_json()?;
-            if content.r#type == "file" {
-                Ok(Content::Text(TextType::Raw(
-                    base64::engine::general_purpose::STANDARD.decode(content.content)?,
-                )))
-            } else {
-                bail!("Unknown Gitea content type: {}", content.r#type);
+        match path {
+            Path::Src(owner, repo, filepath, r#ref) => {
+                let content: ContentsResponse = agent
+                    .request_url(
+                        "GET",
+                        &api_base.join(&format!("repos/{owner}/{repo}/contents{filepath}"))?,
+                    )
+                    .query("ref", r#ref)
+                    .call()?
+                    .into_json()?;
+                if content.r#type == "file" {
+                    Ok(Content::Text(TextType::Raw(
+                        base64::engine::general_purpose::STANDARD.decode(content.content)?,
+                    )))
+                } else {
+                    bail!("Unknown Gitea content type: {}", content.r#type);
+                }
             }
-        } else {
-            bail!("Unknown Gitea URL");
         }
     })())
 }
