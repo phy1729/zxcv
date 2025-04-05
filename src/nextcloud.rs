8032776c556da8525ceb6bc67981caf7f15ef8ce
Matthew Martin <phy1729@gmail.com>
2025-04-05 11:50:59 -0500

    nextcloud: Add support for sharingToken pages

--- src/nextcloud.rs
+++ src/nextcloud.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use base64::Engine;
 use scraper::Html;
 use ureq::Agent;
 use url::Url;
@@ -7,7 +8,7 @@ use crate::html;
 use crate::process_generic;
 use crate::Content;
 
-pub(crate) fn process(agent: &Agent, _: &Url, tree: &Html) -> Option<anyhow::Result<Content>> {
+pub(crate) fn process(agent: &Agent, url: &Url, tree: &Html) -> Option<anyhow::Result<Content>> {
     if html::select_single_element(tree, "meta[name=\"apple-itunes-app\"]")
         .and_then(|e| e.attr("content"))
         != Some("app-id=1125420102")
@@ -15,15 +16,32 @@ pub(crate) fn process(agent: &Agent, _: &Url, tree: &Html) -> Option<anyhow::Res
         return None;
     }
 
-    html::select_single_element(tree, "input#downloadURL").map(|download_input| {
-        process_generic(
-            agent,
-            &Url::parse(
-                download_input
-                    .value()
-                    .attr("value")
-                    .context("downloadURL input missing value")?,
-            )?,
-        )
-    })
+    if let Some(encoded_token) =
+        html::select_single_element(tree, "input#initial-state-files_sharing-sharingToken")
+    {
+        Some((|| {
+            let token: String = encoded_token
+                .value()
+                .attr("value")
+                .context("Missing sharingToken value")
+                .and_then(|v| Ok(base64::engine::general_purpose::STANDARD.decode(v)?))
+                .and_then(|v| Ok(String::from_utf8(v)?))
+                .and_then(|v| Ok(serde_json::from_str(&v)?))
+                .context("Invalid sharingToken")?;
+            let url = url.join("/public.php/dav/files/")?.join(&token)?;
+            process_generic(agent, &url)
+        })())
+    } else {
+        html::select_single_element(tree, "input#downloadURL").map(|download_input| {
+            process_generic(
+                agent,
+                &Url::parse(
+                    download_input
+                        .value()
+                        .attr("value")
+                        .context("downloadURL input missing value")?,
+                )?,
+            )
+        })
+    }
 }
